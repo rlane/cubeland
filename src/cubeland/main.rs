0683c72f018550e7d7dd667bdb67ae40c37c4e8c
Rich Lane <rlane@club.cc.cmu.edu>
2014-01-02 21:54:36 -0800

    rotating cube

--- src/cubeland/main.rs
+++ src/cubeland/main.rs
@@ -1,36 +1,52 @@
 #[feature(globs)];
 
+extern mod extra;
 extern mod glfw;
 extern mod gl;
+extern mod cgmath;
 
 use std::cast;
 use std::ptr;
 use std::str;
 use std::vec;
 use std::libc;
+use extra::time;
 
 use gl::types::*;
+
+use cgmath::matrix::Matrix;
+use cgmath::matrix::Mat3;
+use cgmath::matrix::Mat4;
+use cgmath::matrix::ToMat4;
+use cgmath::vector::Vec4;
+use cgmath::angle::rad;
+use cgmath::projection;
+
+#[link(name="GLU")]
+extern {}
 
 static vertex_shader_src : &'static str = r"
 #version 110
 
+uniform mat4 transform;
+
 attribute vec4 position;
 
-varying vec2 texcoord;
+varying vec3 color;
 
 void main() {
-    gl_Position = position;
-    texcoord = position.xy;
+    gl_Position = transform * position;
+    color = (position.xyz + vec3(1.0, 1.0, 1.0)) * 0.5;
 }
 ";
 
 static fragment_shader_src : &'static str = r"
 #version 110
 
-varying vec2 texcoord;
+varying vec3 color;
 
 void main() {
-    gl_FragColor = vec4(texcoord, 1.0, 1.0);
+    gl_FragColor = vec4(color, 1.0);
 }
 ";
 
@@ -58,17 +74,41 @@ fn main() {
         let fs = compile_shader(fragment_shader_src.as_bytes(), gl::FRAGMENT_SHADER);
         let program = link_program(vs, fs);
 
+        check_gl("after link");
+
+        let mut uniform_transform;
+
+        unsafe {
+            uniform_transform = "transform".with_c_str(|ptr| gl::GetUniformLocation(program, ptr));
+        }
+
+        check_gl("after uniform transform location");
+
         let mut vao = 0;
         let mut vbo = 0;
+        let mut ebo = 0;
 
-        let VERTEX_DATA : ~[GLfloat] = ~[
-            -1.0, -1.0, 0.0, 1.0,
-            1.0, -1.0, 0.0, 1.0,
-            -1.0, 1.0, 0.0, 1.0,
-            1.0, 1.0, 0.0, 1.0,
+        let vertices : ~[GLfloat] = ~[
+            -1.0, -1.0,  1.0, 1.0,
+            1.0, -1.0,  1.0, 1.0,
+            -1.0,  1.0,  1.0, 1.0,
+            1.0,  1.0,  1.0, 1.0,
+            -1.0, -1.0, -1.0, 1.0,
+            1.0, -1.0, -1.0, 1.0,
+            -1.0,  1.0, -1.0, 1.0,
+            1.0,  1.0, -1.0, 1.0,
+        ];
+
+        let elements : ~[GLshort] = ~[
+            0, 1, 2, 3, 7, 1, 5, 4, 7, 6, 2, 4, 0, 1
         ];
 
         unsafe {
+            gl::Enable(gl::DEPTH_TEST);
+            gl::Enable(gl::CULL_FACE);
+
+            glfw::set_swap_interval(1);
+
             // Create Vertex Array Object
             gl::GenVertexArrays(1, &mut vao);
             gl::BindVertexArray(vao);
@@ -77,9 +117,21 @@ fn main() {
             gl::GenBuffers(1, &mut vbo);
             gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
             gl::BufferData(gl::ARRAY_BUFFER,
-                           (VERTEX_DATA.len() * std::mem::size_of::<GLfloat>()) as GLsizeiptr,
-                           cast::transmute(&VERTEX_DATA[0]),
+                           (vertices.len() * std::mem::size_of::<GLfloat>()) as GLsizeiptr,
+                           cast::transmute(&vertices[0]),
                            gl::STATIC_DRAW);
+
+            check_gl("after vertex buffer");
+
+            // Create a Vertex Buffer Object and copy the element data to it
+            gl::GenBuffers(1, &mut ebo);
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+            gl::BufferData(gl::ELEMENT_ARRAY_BUFFER,
+                           (elements.len() * std::mem::size_of::<GLshort>()) as GLsizeiptr,
+                           cast::transmute(&elements[0]),
+                           gl::STATIC_DRAW);
+
+            check_gl("after element buffer");
 
             // Use shader program
             gl::UseProgram(program);
@@ -89,33 +141,73 @@ fn main() {
             gl::EnableVertexAttribArray(vert_attr as GLuint);
             gl::VertexAttribPointer(vert_attr as GLuint, 4, gl::FLOAT,
                                     gl::FALSE as GLboolean, 0, ptr::null());
+
+            check_gl("after vertex attrib pointer");
         }
 
-        check_gl();
+        check_gl("after buffers");
 
         window.set_cursor_pos_callback(~CursorPosContext);
         window.set_key_callback(~KeyContext);
+
+        let start_time = extra::time::precise_time_ns();
+        let mut last_frame_time = start_time;
+        let mut num_frames = 0;
 
         while !window.should_close() {
             glfw::poll_events();
 
             gl::Viewport(0,0, window_width as GLint, window_height as GLint);
 
-            gl::ClearColor(0.8, 0.3, 0.3, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-            gl::DrawArrays(gl::TRIANGLE_STRIP, 0, 4);
+            gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+
+            let elapsed : u64 = extra::time::precise_time_ns() - start_time;
+            let angle : f32 = elapsed as f32 / (1000*1000*1000) as f32;
+
+            let translation = Mat4::<f32>::from_cols(
+                Vec4::<f32>::unit_x(),
+                Vec4::<f32>::unit_y(),
+                Vec4::<f32>::unit_z(),
+                Vec4::<f32>::new(0.0f32, 0.0f32, -5.0f32, 1.0f32));
+            let rotation = Mat3::<f32>::from_angle_y(rad(angle)).to_mat4();
+            let projection = cgmath::projection::perspective(rad(1.57 as f32), (4.0/3.0) as f32, 0.1 as f32, 10.0 as f32);
+            let transform = projection.mul_m(&translation).mul_m(&rotation);
+
+            unsafe {
+                gl::UniformMatrix4fv(uniform_transform, 1, gl::FALSE, cast::transmute(&transform));
+
+                check_gl("after uniform transform");
+
+                gl::DrawElements(gl::TRIANGLE_STRIP, elements.len() as i32, gl::UNSIGNED_SHORT, ptr::null());
+            }
 
             window.swap_buffers();
 
-            check_gl();
+            check_gl("main loop");
+
+            let cur_time = extra::time::precise_time_ns();
+            num_frames += 1;
+            if (cur_time - last_frame_time) > (1000*1000*1000) {
+                println!("{} frames per second", num_frames);
+                num_frames = 0;
+                last_frame_time = cur_time;
+            }
         }
     }
 }
 
-fn check_gl() {
+extern "C" {
+    fn gluErrorString(error: GLenum) -> *GLubyte;
+}
+
+fn check_gl(message : &str) {
     let err = gl::GetError();
     if (err != gl::NO_ERROR) {
-        fail!("GL error");
+        unsafe {
+            let err = std::str::raw::from_c_str(gluErrorString(err) as *i8);
+            fail!("GL error {} at {}", err, message);
+        }
     }
 }
 
