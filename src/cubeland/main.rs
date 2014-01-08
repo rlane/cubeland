4ba22e3ef7e464da282fcd60d1802d1b5066f263
Rich Lane <rlane@club.cc.cmu.edu>
2014-01-08 09:34:41 -0800

    curve the planet

--- src/cubeland/main.rs
+++ src/cubeland/main.rs
@@ -46,8 +46,16 @@ const vec3 light_direction = vec3(0.408248, -0.816497, 0.408248);
 const vec4 light_diffuse = vec4(0.8, 0.8, 0.8, 0.0);
 const vec4 light_ambient = vec4(0.2, 0.2, 0.2, 1.0);
 
+const float planet_radius = 6371000.0 / 10000.0;
+
 void main() {
-    gl_Position = projection * modelview * vec4(position, 1.0);
+    vec4 eye_position = modelview * vec4(position, 1.0);
+
+    /* Curvature of the planet */
+    float distance_squared = pow(eye_position.x, 2) + pow(eye_position.z, 2);
+    eye_position.y -= planet_radius - sqrt(pow(planet_radius, 2) - distance_squared);
+
+    gl_Position = projection * eye_position;
 
     vec4 diffuse_factor
         = max(-dot(normal, light_direction), 0.0) * light_diffuse;
