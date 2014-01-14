fa0a8a111613c9623f77ec11e3a132011b88ffc6
Rich Lane <rlane@club.cc.cmu.edu>
2014-01-14 02:58:41 -0800

    increase number of chunks to compensate for decreased chunk size

--- src/cubeland/main.rs
+++ src/cubeland/main.rs
@@ -50,7 +50,7 @@ mod ratelimiter;
 mod texture;
 mod spiral;
 
-pub static VISIBLE_RADIUS: uint = 4;
+pub static VISIBLE_RADIUS: uint = 8;
 pub static CHUNK_SIZE: uint = 32;
 pub static WORLD_SEED: u32 = 42;
 
