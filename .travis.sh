d36fa5ae30de7f553361bdcbc2825f1eed527a5c
Rich Lane <rlane@club.cc.cmu.edu>
2014-02-02 21:36:02 -0800

    travis: compatibility with older git

--- .travis.sh
+++ .travis.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -eux
 rm -rf rust-gamedev-kit
 git clone git://github.com/rlane/rust-gamedev-kit
-git -C rust-gamedev-kit submodule init
-git -C rust-gamedev-kit submodule deinit rust
-git -C rust-gamedev-kit submodule update
-./rust-gamedev-kit/build-libraries.sh
+pushd rust-gamedev-kit
+git submodule init
+git submodule deinit rust
+git submodule update
+./build-libraries.sh
+popd
 
 export PKG_CONFIG_PATH=./rust-gamedev-kit/install/lib/pkgconfig
 rustc --link-args="`./rust-gamedev-kit/glfw-rs/build/etc/link-args`" -L ./rust-gamedev-kit/install/lib/rustlib/*/lib src/cubeland/main.rs
